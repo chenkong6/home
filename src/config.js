a74e38e1b37291e73083427df1ed4d069557e236
辰空 <2947520194@qq.com>
2026-04-08 21:39:17 +0800

    添加了md.ckong.top链接

--- src/config.js
+++ src/config.js
@@ -145,8 +145,8 @@ const config = {
 
 	//项目卡片 其中 字段"show"控制初始卡片的text是否展开
 	projectcards: [
-		{ go: "🚀 前往", img: "/img/wallpaper/static/射箭少女/45.jpg", title: "Wiki", subtitle: "文章教程都在这里", text: "If you see this line, I've managed to get your attention.", url: "https://ckong.top/#blog", show: false },
-		{ go: "🗂️ 前往", img: "/img/sunshine.jpg", title: "Project 2", subtitle: "2,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ckong.top", show: false },
+		{ go: "🚀 前往", img: "/img/wallpaper/static/射箭少女/45.jpg", title: "Wiki", subtitle: "文章教程都在这里", text: "一般会放一些教程或者碎碎念吧", url: "https://ckong.top/#blog", show: false },
+		{ go: "🗂️ 前往", img: "/img/wallpaper/static/射箭少女/peer.jpg", title: "markdown编辑器", subtitle: "便携的网页版markdown编辑器", text: "功能也许不是很完善，目前处于内侧阶段，想要获取内测资格可以联系我ck@ckong.top", url: "https://md.ckong.top", show: false },
 		{ go: "📝 前往", img: "/img/sunshine.jpg", title: "Project 3", subtitle: "3,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ckong.top", show: false },
 		{ go: "👍 前往", img: "/img/sunshine.jpg", title: "Project 4", subtitle: "4,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ckong.top", show: false },
 		{ go: "🗃 前往", img: "/img/sunshine.jpg", title: "Project 5", subtitle: "5,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ckong.top", show: false },
